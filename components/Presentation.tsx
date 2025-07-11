2376c05e2ff448edc67488c38d37f760671673c5
network <net@d2.plus>
2025-07-12 04:22:53 +0700

    Presentation

---
+++ components/Presentation.tsx
@@ -0,0 +1,12 @@
+export default function Presentation() {
+    return (
+      <div className="relative w-full pb-[56.25%] overflow-hidden">
+        <iframe
+          className="absolute top-0 left-0 w-full h-full"
+          src="https://docs.google.com/presentation/d/e/2PACX-1vQ2ZsM2L_3aA9KbGFKqdy8ZWrJjRInPbtgeV1_yFvjthMlf8t6myKzDEGK9QMBt2JNuZXkXv_Qao1MO/pubembed?start=false&loop=false&delayms=3000"
+          title="Bearium: Settlement Infrastructure for the Onchain Casino Vertical"
+          allowFullScreen
+        />
+      </div>
+    )
+}
